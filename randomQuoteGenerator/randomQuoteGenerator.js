ae6dd9dc435b0063fac97cd1d0ef4a1695b1b6a7
eyitayo <eyitayo@EYITAYO.localdomain>
2026-01-27 15:03:22 +0100

    Completed random quote generator UI

--- randomQuoteGenerator/randomQuoteGenerator.js
+++ randomQuoteGenerator/randomQuoteGenerator.js
@@ -1,4 +1,25 @@
-function getNewRandomQuote () {
+const randomQuoteGeneratorElement = document.getElementById('random-quote-generator')
+const colors = [
+  [ "#FF8080", "#FFCF96" ],
+  [ "#FFF3DA", "#D0BFFF" ],
+  [ "#8DDFCB", "#82A0D8" ],
+  [ "#F9B572", "#99B080" ],
+  [ "#7C96AB", "#BFCCB5" ],
+  [ "#B7B7B7", "#EDC6B1" ],
+  [ "#FFDEB4", "#FFB4B4" ],
+  [ "#FFAACF", "#B9F3E4" ],
+  [ "#B5F1CC", "#E5FDD1" ],
+  [ "#F7ECDE", "#54BAB9" ],
+  [ "#7897AB", "#D885A3" ],
+  [ "#B97A95", "#F6AE99" ],
+]
+function getRandomColorCombo () {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex]
+}
+
+
+function getNewRandomQuote() {
 	fetch('https://api.quotable.io/quotes/random')
   .then(response => {
     if (!response.ok) {
@@ -6,7 +27,9 @@ function getNewRandomQuote () {
     }
     return response.json();
   })
-  .then(data => {
+    .then(data => {
+    const colorCombo = getRandomColorCombo()
+    randomQuoteGeneratorElement.style.background = 'linear-gradient(45deg, ' + colorCombo[0] + ', ' + colorCombo[1]+ ')'
     const quoteText = data[0].content
     const quoteAuthor = data[0].author
     document.getElementById('random-quote-text').innerHTML = quoteText
