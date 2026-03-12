60a926514b7f41f7ecb226f41b7f93a7c3259fa1
eyitayo <eyitayo@EYITAYO.localdomain>
2026-03-12 18:55:43 +0100

    remove redundant index.html copies

--- personal-webiste/src/main.tsx
+++ personal-webiste/src/main.tsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import StockAnalysisDashboard from './stockAnalysisDashboard/stockAnalysisDashboard/stockAnalysisDashboard.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <StockAnalysisDashboard />
-  </StrictMode>,
-)
+// simple root component; you can expand this or import from other files
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Welcome to the Vite React app!</h1>
+      <p>Start editing <code>src/main.tsx</code> to get going.</p>
+    </div>
+  );
+};
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  console.error('Root element not found');
+}
